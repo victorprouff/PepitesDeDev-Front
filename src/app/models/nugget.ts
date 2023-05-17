aee87ad32a8b774aa595fd1af10ee2848e54656f
Victor Prouff <contact@victorprouff.fr>
2023-05-17 13:33:26 +0200

    Navbar en fonction de la connexion d'un user

--- src/app/models/nugget.ts
+++ src/app/models/nugget.ts
@@ -3,13 +3,13 @@ import { Guid } from "guid-typescript";
 export class Nugget {
     id: Guid;
     title: string;
-    description: string;
+    content: string;
     createdAt: Date
 
-    constructor(id: Guid, title: string, description: string, createdAt: Date){
+    constructor(id: Guid, title: string, content: string, createdAt: Date){
         this.id = id;
         this.title = title;
-        this.description = description;
+        this.content = content;
         this.createdAt = createdAt;
     }
 }
