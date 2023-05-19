df39d1889d352ee73ca262b00dd125c8591daf3f
Victor Prouff <contact@victorprouff.fr>
2023-05-19 09:08:37 +0200

    Add userId on nugget.ts

--- src/app/models/nugget.ts
+++ src/app/models/nugget.ts
@@ -2,12 +2,14 @@ import { Guid } from "guid-typescript";
 
 export class Nugget {
     id: Guid;
+    userId: Guid;
     title: string;
     content: string;
     createdAt: Date
 
-    constructor(id: Guid, title: string, content: string, createdAt: Date){
+    constructor(id: Guid, userId: Guid, title: string, content: string, createdAt: Date){
         this.id = id;
+        this.userId = userId;
         this.title = title;
         this.content = content;
         this.createdAt = createdAt;
