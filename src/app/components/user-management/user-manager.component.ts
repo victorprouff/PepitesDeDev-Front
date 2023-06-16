8a179efe0efdd206c9d69922a9ca8ed5bcd32dde
Victor Prouff <contact@victorprouff.fr>
2023-06-16 20:55:19 +0200

    Personal nuggets and all nuggets in differents cards

--- src/app/components/user-management/user-manager.component.ts
+++ src/app/components/user-management/user-manager.component.ts
@@ -9,11 +9,13 @@ import {User} from "../../models";
 export class UserManagerComponent {
     active = 1;
     user?: User | null;
+    userIsAdmin = false;
 
     constructor(
         private authenticationService: AuthenticationService,
     ) {
         this.user = this.authenticationService.GetUserFromToken;
+        this.userIsAdmin = this.authenticationService.GetUserFromToken?.isAdmin || false;
     }
 
 }
