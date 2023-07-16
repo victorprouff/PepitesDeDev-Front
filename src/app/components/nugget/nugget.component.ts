11331e4f73d565a7d37e6d2bff6d5f7b76f03dba
Victor Prouff <contact@victorprouff.fr>
2023-07-16 10:48:39 +0200

    Rollback

--- src/app/components/nugget/nugget.component.ts
+++ src/app/components/nugget/nugget.component.ts
@@ -1,20 +1,23 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnDestroy} from '@angular/core';
 import {Nugget} from '../../models';
 import {AuthenticationService, NuggetService} from '../../services';
 import {ActivatedRoute} from "@angular/router";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {RedirectService} from "../../services/redirect.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-nugget',
     templateUrl: './nugget.component.html'
 })
-export class NuggetComponent {
+export class NuggetComponent implements OnDestroy{
     Activatedroute = inject(ActivatedRoute)
     nuggetService = inject(NuggetService)
     redirect = inject(RedirectService)
     authenticationService = inject(AuthenticationService)
     modalService = inject(NgbModal)
+
+    subscriptions: Subscription[] = []
 
     userId: string = '';
     userIsAdmin = false;
@@ -32,35 +35,43 @@ export class NuggetComponent {
         this.userId = this.authenticationService.GetUserFromToken?.id || ''
         this.userIsAdmin = this.authenticationService.GetUserFromToken?.isAdmin || false;
 
-        this.Activatedroute.paramMap.subscribe(paramMap => {
+        const subscription = this.Activatedroute.paramMap.subscribe(paramMap => {
             const id = paramMap.get('id') || '';
             this.getNugget(id);
         });
+
+        this.subscriptions.push(subscription)
     }
 
     getNugget(id: string) {
-        this.nuggetService.get(id)
+        const subscription = this.nuggetService.get(id)
             .subscribe(nugget => {
                     this.nugget = nugget;
                 },
                 error => {
                     this.redirect.toHome()
                 });
+
+        this.subscriptions.push(subscription)
     }
 
     getNuggets() {
-        this.nuggetService.getList(this.itemsPerPage, (this.currentPage - 1) * this.itemsPerPage)
+        const subscription = this.nuggetService.getList(this.itemsPerPage, (this.currentPage - 1) * this.itemsPerPage)
             .subscribe(
                 (result) => {
                     this.nugget = result.nuggets.map((n) => new Nugget(n.id, n.userId, n.title, n.content, n.urlImage, n.creator, n.createdAt))[0];
                     this.totalItemsPages = result.nbOfNuggets
                     this.nbPage = this.getNbOfPage(this.totalItemsPages)
                 });
+
+        this.subscriptions.push(subscription)
     }
 
     delete() {
-        this.nuggetService.delete(this.deleteNuggetId)
+        const subscription = this.nuggetService.delete(this.deleteNuggetId)
             .subscribe(_ => this.ngOnInit());
+
+        this.subscriptions.push(subscription)
     }
 
     update(id: string) {
@@ -85,5 +96,9 @@ export class NuggetComponent {
 
     getNbOfPage(nbOfNuggets: number) {
         return Math.ceil(nbOfNuggets / this.itemsPerPage);
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach((subscription) => subscription.unsubscribe())
     }
 }
